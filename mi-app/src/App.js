a898016b4d1486b7886313356ebe195fe130dfaf
oscartrugo <oscartrugo@gmail.com>
2022-03-28 00:21:19 -0600

    24. Estilo con clases de CSS

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import './main.css'
 
 const estilo2 = {
   boxShadow: '0 5px 3px rgba(0,0,0,0.5)'
@@ -15,14 +16,14 @@ const estilo = ({bg = '#222'}) => ({
 //const App = () => { es otra forma de declarar un componente
 const Li = ({children}) =>{
   return(
-    <li style={{...estilo2, ...estilo({bg: '#333'}) }} className='clase-li'>{children}</li>
+    <li className='clase-li'>{children}</li>
   )
 }
 
 function App() {
   const valor = 'triste'
   return (
-    <ul style={estilo({bg: '#750'})} className='clase-css'>
+    <ul className='clase-css'>
       <Li estado='feliz'>Valor de li children</Li> {/** Todo lo que vaya entre etiquetas pasa a ser el valor de children */}
     </ul> //className sirven para asignar clases de css
   );
