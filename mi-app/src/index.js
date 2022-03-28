c02464a11dd10560c075651ea066de038e095b4d
oscartrugo <oscartrugo@gmail.com>
2022-03-27 22:54:30 -0600

    20. Introduccion a JSX

--- mi-app/src/index.js
+++ mi-app/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const X = () => <p>Hola mundo </p> //COMPONENTE FUNCIONAL = ES UNA FUNCIÓN
+// <p>Hola mundo </p>
+
+ReactDOM.render( //ReacDOM.render ES EL ENCARGADO DE RENDERIZAR LA APP
+ <X/>, document.getElementById('root') //qué queremos renderizar, dónde queremos renderizarlo
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
