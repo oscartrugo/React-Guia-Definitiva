77b755f8363abfbc6101d0c17b4f5df80c5ee941
oscartrugo <oscartrugo@gmail.com>
2022-03-28 02:22:17 -0600

    34. Montando componentes

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,6 +1,16 @@
 import { Component } from "react"
 
 class Button extends Component{
+  state={}
+  constructor(props){ 
+    super(props)
+    console.log('constructor', props);
+  }
+
+  componentDidMount(){ //Este método se utiliza para poder actualizar el estado de nuestro componente
+    console.log("componentDidMount") //Este método es lo último que se ejecuta
+  }
+  
   render(){ //Método obligatorio, todos los componentes deben llevar su render
     console.log("Ejecutando método render de button")
     return(
@@ -20,7 +30,7 @@ class App extends Component{ //Poviene de Component
     return(
       <div>
         <p>Hola mundo</p>
-        <Button/> {/**Llama a su método render y lo ejecuta sea que sus props hayan cambiado o no */}
+        <Button chanchito='feliz'/> {/**Llama a su método render y lo ejecuta sea que sus props hayan cambiado o no */}
         <button className={`${this.state.valor}`} onClick={() => this.setState({ valor: 2})}> {/**cambiamos el state con setState y se ejecuta el método render */}
           Enviar en App
         </button>
@@ -41,4 +51,8 @@ export default App
  * El método RENDER se ejecuta siempre en dos condiciones:
  * 1. El estado del componente cambió. 
  * 2. Cuando el método render de un componente padre ha sido llamado
+ * 
+ * El CONSTRUCTOR es la instancia para que podamos setear un estado de los componentes en React,
+ * pero eso es una mala práctica. Puede producir errores en la app debido a que las props pueden
+ * estar cambiando
  */
