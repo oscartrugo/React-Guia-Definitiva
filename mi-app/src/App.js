e7e2faac07ec7cb83fac7c6bde3543e35287500b
oscartrugo <oscartrugo@gmail.com>
2022-03-28 00:45:52 -0600

    27. Renderizado condicional

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -1,6 +1,11 @@
 import Button from "./Button"
 
 const App = () =>{
+  const miVariable = true
+
+  if(miVariable){ //Renderiza si la condición se cumple, si no renderiza el siguiente return
+    return <p>Mi vaiable dio true</p>
+  }
   return (
     <div>
       <h1 onClick={(e) => console.log('click',e)}>Hola mundo</h1> {/** Le pasamos una funcion */}
