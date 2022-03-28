8c17ce8438af616e2354be2caa897a4e82fb9a5c
oscartrugo <oscartrugo@gmail.com>
2022-03-28 00:42:49 -0600

    26. Componentes interactivos

--- mi-app/src/App.js
+++ mi-app/src/App.js
@@ -3,7 +3,7 @@ import Button from "./Button"
 const App = () =>{
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <h1 onClick={(e) => console.log('click',e)}>Hola mundo</h1> {/** Le pasamos una funcion */}
       <Button onClick={() => console.log('clickeado')}>Enviarrr</Button>
     </div>
   )
