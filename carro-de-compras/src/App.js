e084bb9af798d201227d82333c5924ed32158e1c
oscartrugo <oscartrugo@gmail.com>
2022-03-28 09:02:56 -0600

    41. Estado inicial y cargando imágenes

--- carro-de-compras/src/App.js
+++ carro-de-compras/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  state = { //Declaramos el estado
+    productos: [ //Propiedad "productos" es un arreglo de objetos
+      {name: "Tomate", price: 1500, img: '/productos/tomate.jpg'},
+      {name: "Arbejas", price: 2500, img: '/productos/arbejas.jpg'},
+      {name: "Lechuga", price: 500, img: '/productos/lechuga.jpg'}
+
+    ]
+  }
   render(){
     return(
       <div>
