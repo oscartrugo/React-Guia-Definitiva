f61eea0983a9e842e8e669eadd941ea4171f5030
oscartrugo <oscartrugo@gmail.com>
2022-03-28 11:47:49 -0600

    45. Componente de layout

--- carro-de-compras/src/App.js
+++ carro-de-compras/src/App.js
@@ -1,22 +1,25 @@
 import { Component } from 'react';
 import Productos from './components/Productos'
+import Layout from './components/Layout'
 
-class App extends Component{
+class App extends Component {
   state = { //Declaramos el estado
     productos: [ //Propiedad "productos" es un arreglo de objetos
-      {name: "Tomate", price: 1500, img: '/productos/tomate.jpg'},
-      {name: "Arbejas", price: 2500, img: '/productos/arbejas.jpg'},
-      {name: "Lechuga", price: 500, img: '/productos/lechuga.jpg'}
+      { name: "Tomate", price: 1500, img: '/productos/tomate.jpg' },
+      { name: "Arbejas", price: 2500, img: '/productos/arbejas.jpg' },
+      { name: "Lechuga", price: 500, img: '/productos/lechuga.jpg' }
 
     ]
   }
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <Productos
-        agregarAlCarro={() =>  console.log("No hace nada")} //Le pasamos una función al componente
-        productos={this.state.productos} //Le pasamos todos los productos que queremos imprimir
-        />
+        <Layout>
+          <Productos
+            agregarAlCarro={() => console.log("No hace nada")} //Le pasamos una función al componente
+            productos={this.state.productos} //Le pasamos todos los productos que queremos imprimir
+          />
+        </Layout>
       </div>
     )
   }
---
+++ carro-de-compras/src/components/Layout.js
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+const styles = {
+    layout: {
+        backgroundColor: '#fff',
+        color: '#0a283',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    container: {
+        width: '800px'
+    }
+}
+
+class Layout extends Component{
+    render(){
+        return(
+            <div style={styles.layout}>
+                <div style={styles.container}>
+                    {this.props.children} {/**Se imprimen los productos dentro del layout */}
+                    lala
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Layout
