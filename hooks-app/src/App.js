30ca234ca748577124f8236c89e4f8d3bd7bec2f
oscartrugo <oscartrugo@gmail.com>
2022-03-29 15:37:07 -0600

    64. useRef

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -1,47 +1,32 @@
-import { useState, useEffect, Component } from "react"
+import { useReducer, useState } from "react";
 
-const useContador = (inicial) => { //Definimos un Hook customizado 
-  const [contador, setContador] = useState(inicial) //useState recibe el valor inicial de nuestro contador
-  const incrementar = () => {
-    setContador(contador + 1)
+//state {contador: 0}
+//action siempre va a ser un objeto que va a contener una propiedad de type, y este va a ser un string
+//action = {type: string, payload: any}
+const inicial = {contador: 0}
+const reducer = (state, action) =>{
+  switch(action.type){ //Evaluamos el valor de la propiedad type
+    case 'incrementar':
+      return {contador: state.contador + 1}
+    case 'decrementar':
+      return {contador: state.contador - 1}
+    case 'set': //En lugar de manipular el valor
+      return {contador: action.payload} //Le colocamos el valor dentro de la propiedad payload
+    default: 
+    return state
   }
-
-  return [contador, incrementar]
 }
 
-// const Interval = ({contador}) => {
-//   useEffect(() => {
-//     const i = setInterval(() => console.log(contador), 1000)
-//     return () => clearInterval(i) //Cada que se ejecute el componente, desuscribimos el intervalor
-//   }, [contador])
-//   return (
-//     <p>Intervalo</p>
-//   )
-// }
-class Interval extends Component{
-  intervalo = ''
-  componentDidMount(){
-    this.intervalo = setInterval(() => console.log(this.props.contador), 1000)
-  }
-  componentWillUnmount(){
-    clearInterval(this.intervalo)
-  }
-  render(){
-    return(
-      <p>Intervalo</p>
-    )
-  }
-}
-const App = () => { // [elemento, funcion]
-  const [contador, incrementar] = useContador(0) //useState recibe el valor inicial de nuestro contador
-  useEffect(() => {
-    document.title=contador
-  }, [contador]) //Todo lo de dentro del hook no tiene dependencias
+const App = () =>{
+  const [state, dispatch] = useReducer(reducer, inicial)//Recibe un estado inicial y el reducer
+  const [valor, setValor] = useState(0)
   return(
     <div>
-      Contador: {contador}
-      <button onClick={incrementar}>Incrementar</button>
-      <Interval contador={contador}/>
+      Contador: {state.contador}
+      <input value={valor} onChange={e => setValor(e.target.value)}/>
+      <button onClick={() => dispatch({ type: 'incrementar' })}>Más</button>
+      <button onClick={() => dispatch({ type: 'decrementar' })}>Menos</button>
+      <button onClick={() => dispatch({ type: 'set', payload: valor })}>Set</button>
     </div>
   )
 }
---
+++ hooks-app/src/useEffect.js
@@ -0,0 +1,49 @@
+import { useState, useEffect, Component } from "react"
+
+const useContador = (inicial) => { //Definimos un Hook customizado 
+  const [contador, setContador] = useState(inicial) //useState recibe el valor inicial de nuestro contador
+  const incrementar = () => {
+    setContador(contador + 1)
+  }
+
+  return [contador, incrementar]
+}
+
+// const Interval = ({contador}) => {
+//   useEffect(() => {
+//     const i = setInterval(() => console.log(contador), 1000)
+//     return () => clearInterval(i) //Cada que se ejecute el componente, desuscribimos el intervalor
+//   }, [contador])
+//   return (
+//     <p>Intervalo</p>
+//   )
+// }
+class Interval extends Component{
+  intervalo = ''
+  componentDidMount(){
+    this.intervalo = setInterval(() => console.log(this.props.contador), 1000)
+  }
+  componentWillUnmount(){
+    clearInterval(this.intervalo)
+  }
+  render(){
+    return(
+      <p>Intervalo</p>
+    )
+  }
+}
+const App = () => { // [elemento, funcion]
+  const [contador, incrementar] = useContador(0) //useState recibe el valor inicial de nuestro contador
+  useEffect(() => {
+    document.title=contador
+  }, [contador]) //Todo lo de dentro del hook no tiene dependencias
+  return(
+    <div>
+      Contador: {contador}
+      <button onClick={incrementar}>Incrementar</button>
+      <Interval contador={contador}/>
+    </div>
+  )
+}
+
+export default useEffect
