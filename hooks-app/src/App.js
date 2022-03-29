366a6432c4318e336885ed7af7f27c03491d13ff
oscartrugo <oscartrugo@gmail.com>
2022-03-29 12:17:13 -0600

    60. useEffect

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -1,19 +1,5 @@
-import { useState } from "react" 
+import { useState, useEffect } from "react"
 
-// class App extends Component{
-//   state = {contador: 0}
-//   incrementar = () =>{
-//     this.setState({contador: this.state.contador + 1})
-//   }
-//   render(){
-//     return(
-//       <div>
-//       Contador: {this.state.contador}
-//       <button onClick={this.incrementar}>Incrementar</button>
-//     </div>
-//     )
-//   }
-// }
 const useContador = (inicial) => { //Definimos un Hook customizado 
   const [contador, setContador] = useState(inicial) //useState recibe el valor inicial de nuestro contador
   const incrementar = () => {
@@ -24,6 +10,9 @@ const useContador = (inicial) => { //Definimos un Hook customizado
 }
 const App = () => { // [elemento, funcion]
   const [contador, incrementar] = useContador(0) //useState recibe el valor inicial de nuestro contador
+  useEffect(() => {
+    document.title=contador
+  }, [contador]) //Todo lo de dentro del hook no tiene dependencias
   return(
     <div>
       Contador: {contador}
@@ -33,20 +22,3 @@ const App = () => { // [elemento, funcion]
 }
 
 export default App
-
-/**
- * 
- * NO PODEMOS LLAMAR A LOS HOOKS EN COMPONENTES DE CLASES
- * 
- * REGLAS DE LOS HOOKS
- * 
- * 1. Los hooks no se llaman en loops (while, for, if).
- * Necesariamente debe ser al comieno del componente, en el nivel más alto de la función.
- * NO PUEDE estar dentro de nada, debe estar en el nivel más alto.
- * 
- * 2. Sólo se llaman en 2 partes:
- *    -Componentes de React.
- *    -custom hooks
- * 
- * 3. Cuando creemos un custom hook, este siempre tiene que comenzar con el nombre use*
- */
---
+++ hooks-app/src/useState.js
@@ -0,0 +1,57 @@
+import { useState } from "react" 
+
+// class App extends Component{
+//   state = {contador: 0}
+//   incrementar = () =>{
+//     this.setState({contador: this.state.contador + 1})
+//   }
+//   render(){
+//     return(
+//       <div>
+//       Contador: {this.state.contador}
+//       <button onClick={this.incrementar}>Incrementar</button>
+//     </div>
+//     )
+//   }
+// }
+const useContador = (inicial) => { //Definimos un Hook customizado 
+  const [contador, setContador] = useState(inicial) //useState recibe el valor inicial de nuestro contador
+  const incrementar = () => {
+    setContador(contador + 1)
+  }
+
+  return [contador, incrementar]
+}
+const App = () => { // [elemento, funcion]
+  const [contador, incrementar] = useContador(0) //useState recibe el valor inicial de nuestro contador
+  return(
+    <div>
+      Contador: {contador}
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  )
+}
+
+export default useState
+
+/**
+ * 
+ * NO PODEMOS LLAMAR A LOS HOOKS EN COMPONENTES DE CLASES
+ * 
+ * REGLAS DE LOS HOOKS
+ * 
+ * 1. Los hooks no se llaman en loops (while, for, if).
+ * Necesariamente debe ser al comieno del componente, en el nivel más alto de la función.
+ * NO PUEDE estar dentro de nada, debe estar en el nivel más alto.
+ * 
+ * 2. Sólo se llaman en 2 partes:
+ *    -Componentes de React.
+ *    -custom hooks
+ * 
+ * 3. Cuando creemos un custom hook, este siempre tiene que comenzar con el nombre use*
+ * 
+ * useEffect
+ * Cada vez queramos generar un efecto lateral (actualizar dom, escribir o leer db)
+ * Cada vez que queramos generar algo que afecte el uso de nuestro componente se utiliza useEffect
+ */
+
