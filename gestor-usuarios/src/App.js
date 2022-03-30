bcc2eb62816d9b287f7e72b67738be4f7dc892fc
oscartrugo <oscartrugo@gmail.com>
2022-03-30 16:40:12 -0600

    77. Componente Card

--- gestor-usuarios/src/App.js
+++ gestor-usuarios/src/App.js
@@ -2,16 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import useFormulario from './hooks/useFormulario'
 import Input from './components/Input'
+import Card from './components/Card'
 
 function App() {
-  const [formulario, handleChange] = useFormulario({name: '', lastName: ''}) //Valores iniciales
-   console.log(formulario)
+  const [formulario, handleChange] = useFormulario({ name: '', lastName: '' }) //Valores iniciales
+  console.log(formulario)
   return (
-    <form>
-      <Input label='Nombre' name='name' value={formulario.name} onChange={handleChange}/>
-      <Input label='Apellido' name='lastName' value={formulario.lastName} onChange={handleChange}/>
+    <Card>
+      <form>
+        <Input
+          label='Nombre'
+          name='name'
+          value={formulario.name}
+          onChange={handleChange} />
+        <Input
+          label='Apellido'
+          name='lastName'
+          value={formulario.lastName}
+          onChange={handleChange} />
 
-    </form>
+      </form>
+    </Card>
   )
 }
 
