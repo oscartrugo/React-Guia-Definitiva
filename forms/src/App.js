0cf6387f26ffea648a1bf75a90cf80e90c337e16
oscartrugo <oscartrugo@gmail.com>
2022-03-30 02:28:15 -0600

    71. Etiqueta de Select

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 const App = () => {
-    const [value, setValue] = useState({normal: 'Por defecto', texto: 'Valor inicial'}) //...Actualizamos este objeto
+    const [value, setValue] = useState({normal: 'Por defecto', texto: 'Valor inicial', select: ''}) //...Actualizamos este objeto
     const handleChange = (e) =>{
         setValue((state) => ({
             ...state,
@@ -17,6 +17,14 @@ const App = () => {
             {value.length < 5 ? <span>longitud minima de 5</span> : null}
             <input type='text' name='normal' value={value.normal} onChange={handleChange}/> {/**Un controlled component necesitamos pasarle el valor que va a mostrar */}
             <textarea name='texto' onChange={handleChange} value={value.texto}/> {/**Le asignamos elt exto en value */}
+
+            <select vqlaue={value.select} name='select' onChange={handleChange}>
+                <option value=''>-- Seleccione --</option>
+                <option value='chanchofeliz'>Chancho feliz</option>
+                <option value='chanchitofeliz'>Chanchito feliz</option>
+                <option value='chanchitotriste'>Chanchito triste</option>
+                <option value='felipe'>Felipe</option>
+            </select>
         </div>
     )
 }
