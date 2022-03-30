f641a7ee70acf3a48755d0b4b76e3fa4482c8f67
oscartrugo <oscartrugo@gmail.com>
2022-03-30 02:51:45 -0600

    73. Radio button

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,12 +1,18 @@
 import { useState } from "react"
 
 const App = () => {
-    const [value, setValue] = useState({normal: 'Por defecto', texto: 'Valor inicial', select: '', check: false}) //...Actualizamos este objeto
-    const handleChange = ({target}) =>{
+    const [value, setValue] = useState({ 
+        normal: 'Por defecto', 
+        texto: 'Valor inicial', 
+        select: '', 
+        check: false,
+        estado: 'feliz'
+    }) //...Actualizamos este objeto
+    const handleChange = ({ target }) => {
         setValue((state) => ({
             ...state,
-            [target.name]: target.type === 'checkbox' 
-                ? target.checked 
+            [target.name]: target.type === 'checkbox'
+                ? target.checked
                 : target.value
         }))
         // setValue({
@@ -14,11 +20,11 @@ const App = () => {
         //     [e.target.name]: e.target.value}) //La propiedad va a tener el valor del campo que estemos modificando, con ello ...
     }
     console.log(value)
-    return(
+    return (
         <div>
             {value.length < 5 ? <span>longitud minima de 5</span> : null}
-            <input type='text' name='normal' value={value.normal} onChange={handleChange}/> {/**Un controlled component necesitamos pasarle el valor que va a mostrar */}
-            <textarea name='texto' onChange={handleChange} value={value.texto}/> {/**Le asignamos elt exto en value */}
+            <input type='text' name='normal' value={value.normal} onChange={handleChange} /> {/**Un controlled component necesitamos pasarle el valor que va a mostrar */}
+            <textarea name='texto' onChange={handleChange} value={value.texto} /> {/**Le asignamos elt exto en value */}
 
             <select vqlaue={value.select} name='select' onChange={handleChange}>
                 <option value=''>-- Seleccione --</option>
@@ -34,6 +40,31 @@ const App = () => {
                 onChange={handleChange}
                 checked={value.check}
             />
+
+            <div> {/**Obtenemos el valor del radio por la etiqueta padre */}
+                <label>Chancho</label>
+                <input
+                    onChange={handleChange}
+                    type='radio'
+                    value='feliz'
+                    name="estado"
+                    checked={value.estado === 'feliz'}
+                /> Feliz
+                <input
+                    onChange={handleChange}
+                    type='radio'
+                    value='triste'
+                    name="estado"
+                    checked={value.estado === 'triste'}
+                /> Triste
+                <input
+                    onChange={handleChange}
+                    type='radio'
+                    value='felipe'
+                    name="estado" 
+                    checked={value.estado === 'felipe'}
+                    /> Felipe
+            </div>
         </div>
     )
 }
