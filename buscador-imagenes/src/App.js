19d479743df69edaca1cb5b04bd9c88c77a0fe43
oscartrugo <oscartrugo@gmail.com>
2022-04-03 23:30:28 -0500

    99. Iterando imagenes

--- buscador-imagenes/src/App.js
+++ buscador-imagenes/src/App.js
@@ -4,6 +4,7 @@ import './header.css'
 
 const App = () => {
   const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
   console.log({photos})
   return (
     <div>
@@ -26,6 +27,16 @@ const App = () => {
           </Form>
           </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular}/>
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>
+            )}
+        </div>
+      </div>
     </div>
   )
 }
