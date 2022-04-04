9128f7dfbe5e91ead3806eae31a1729b2b20d243
oscartrugo <oscartrugo@gmail.com>
2022-04-04 13:58:47 -0500

    118. formula de interés compuesto

--- interes-compuesto/src/App.js
+++ interes-compuesto/src/App.js
@@ -5,11 +5,20 @@ import Button from './components/Button'
 import Container from './components/Container';
 import Section from './components/Section';
 
+const compoundInterest = (deposit, contribution, years, rate) => {
+  let total = deposit
+  for(let i=0; i<years; i++){
+    total = (total + contribution) * (rate + 1)
+  }
+
+  return Math.round(total)
+}
 
 function App() {
 
-  const handleSubmit = () => {
-
+  const handleSubmit = ({deposit, contribution, years, rate}) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    console.log(val)
   }
 
   return (
