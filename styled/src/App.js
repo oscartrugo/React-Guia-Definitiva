6a3c9761a2531245371bfc386660bc455bd0d143
oscartrugo <oscartrugo@gmail.com>
2022-04-04 09:10:47 -0500

    102. Styled components - Instalacion y funcionamiento

---
+++ styled/src/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+
+`
+function App() {
+  return (
+    <div>
+      <P>Hola mundo</P>
+    </div>
+  )
+}
+
+export default App;
