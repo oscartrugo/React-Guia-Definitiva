493ab9e71686ce4493eb98ff13a5b5e1dda76a87
oscartrugo <oscartrugo@gmail.com>
2022-04-04 12:32:15 -0500

    115. Creando una tarjeta gris

--- interes-compuesto/src/App.js
+++ interes-compuesto/src/App.js
@@ -7,10 +7,20 @@ const Container = styled.div`
   align-items: center;
 `
 
+const Section = styled.section`
+  background-color: #eee;
+  border-top: solid 2px palevioletred;
+  padding: 20px 25px;
+  width: 500px;
+  box-shadow: 0px 2px 3px rgb(0,0,0,0.3);
+`
+
 function App() {
   return (
     <Container>
-      lala
+      <Section>
+        Lala
+      </Section>
     </Container>
   );
 }
