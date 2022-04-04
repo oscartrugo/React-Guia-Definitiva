e8eef07fb224020e16e552be53ce064feb767927
oscartrugo <oscartrugo@gmail.com>
2022-04-03 22:49:28 -0500

    97. Header de la aplicacion

---
+++ buscador-imagenes/src/App.js
@@ -0,0 +1,24 @@
+import { Formik, Form, Field } from "formik";
+import './header.css'
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <Formik
+          initialValues = {{ search: ''}}
+          onSubmit={async values => {
+            //Llamar a la api de unsplash
+            console.log(values)
+          }}
+        >
+          <Form>
+            <Field name='search'/>
+          </Form>
+          </Formik>
+      </header>
+    </div>
+  )
+}
+
+export default App;
