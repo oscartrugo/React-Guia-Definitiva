a619a4521fc0d90bf0d3d95df2f3d8fd809406bc
oscartrugo <oscartrugo@gmail.com>
2022-04-05 16:23:30 -0500

    147. Mas ejemplos de Redux

--- red_ux/src/index.js
+++ red_ux/src/index.js
@@ -6,21 +6,36 @@ import reportWebVitals from './reportWebVitals';
 //Esta función se ejecuta siempre que creamos nuestro store
 const store = createStore((state = 0, action) => { //Es un reducer (acumulador, elemento) | (estado, accion) = state, action
   switch(action.type){
-    case 'accion': {
-      return action.payload //Payload nos permite cambiar el valor de nuestro estado
+    case 'incrementar': {
+      return state + 1 //Payload nos permite cambiar el valor de nuestro estado
     }
+    case 'decrementar': {
+      return state - 1
+    }
+    case 'set': {
+      return action.payload
+    }
+    default:
+      return state;
   }
   
   //action = { type: 'tipo de accion', payload: any}
   //console.log({state, action}) //state = datos de la aplicación
-  return state //Store almacena muchos metodos para interactuar con redux, recibe una funcion
+ //Store almacena muchos metodos para interactuar con redux, recibe una funcion
 })
 
 //console.log(store.getState()) //getState devuelve siempre el estado completo de la app cuando se llama desde el store
 //Todas las acciones que despachemos en redux tienen que tener la propiedad de type
-store.dispatch({ type: 'accion', payload: 2 }) //Cada que llamemos al dispatch desde el store, la funcion reducer se vuelve a ejecutar
-store.dispatch({ type: 'accionindefinida', payload: 15 }) //Cada que llamemos al dispatch desde el store, la funcion reducer se vuelve a ejecutar
 console.log(store.getState())
+store.dispatch({ type: 'lala' }) //Cada que llamemos al dispatch desde el store, la funcion reducer se vuelve a ejecutar
+console.log(store.getState())
+store.dispatch({ type: 'incrementar' }) //Cada que llamemos al dispatch desde el store, la funcion reducer se vuelve a ejecutar
+console.log(store.getState())
+store.dispatch({ type: 'decrementar' }) //Cada que llamemos al dispatch desde el store, la funcion reducer se vuelve a ejecutar
+console.log(store.getState())
+store.dispatch({ type: 'set', payload: 15 }) 
+console.log(store.getState())
+//store.dispatch({ type: 'accionindefinida', payload: 15 }) //Cada que llamemos al dispatch desde el store, la funcion reducer se vuelve a ejecutar
 
 ReactDOM.render(
   <React.StrictMode>
