9b41dd892a610a3c580b94df4311accac5be8c00
oscartrugo <oscartrugo@gmail.com>
2022-04-04 21:35:16 -0500

    135. Consumir multiples context

--- ctx/src/App.js
+++ ctx/src/App.js
@@ -1,34 +1,46 @@
-import { Component, createContext } from "react";
-
-const Context = createContext('mi valor')
-
-const Provider = ({ children }) => {
-    return (
-        <Context.Provider value="otro valor">
-            {children}
-        </Context.Provider>
+import { createContext, Component, useContext } from "react"; //Importamos
+//Creamos los contextos
+const Context1 = createContext('mi primer context')
+const Context2 = createContext('mi segundo context')
+//Creamos un provider que recibe un children y lo imprime
+const Provider = ({children}) => {
+    return(
+        <Context1.Provider value="valor 1">
+            <Context2.Provider value="valor 2">
+                {children}
+            </Context2.Provider>
+        </Context1.Provider>
     )
 }
 
 class Componente extends Component {
-    //static contextType = Context
-    render() {
-        console.log(this.context)
-        return (
-            <div>Hola mundo</div>
+    render(){
+        return(
+            <Context1.Consumer>
+                {
+                    valor1 => 
+                    <Context2.Consumer>
+                        {valor2 => <div>{`${valor1} ${valor2}`}</div>}
+                    </Context2.Consumer>
+                }
+            </Context1.Consumer>
         )
     }
 }
 
-Componente.contextType = Context
+const Componente2 = () => {
+    const valor1 = useContext(Context1)
+    const valor2 = useContext(Context2)
+    return(
+        <div>{`${valor1} ${valor2}`}</div>
+    )
+}
 
-const App = () => {
-    return (
+const App = () => { //Retorna provider y dentro un componente basado en clases
+    return(
         <Provider>
-            <Componente />
-            <Context.Consumer>
-                {valor => <div>{valor}</div>}
-            </Context.Consumer>
+            <Componente/>
+            <Componente2/>
         </Provider>
     )
 }
