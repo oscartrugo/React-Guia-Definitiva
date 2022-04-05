4059a485b10c73b9791c5f7fa6214c10d2d31136
oscartrugo <oscartrugo@gmail.com>
2022-04-05 00:31:04 -0500

    139. Rutas variables

--- router/src/2-nested.js
+++ router/src/2-nested.js
@@ -1,12 +1,4 @@
-import { Route, Routes, Link, useMatch, useParams } from 'react-router-dom';
- 
-const Proyecto = () => {
-  const match = useMatch('/portafolio/:proyecto_id');
-  const params = useParams(); //Hook para saber los parámetros
-  const { proyecto_id } = params;
-  console.log({ match, params });
-  return <h2>Proyecto: {proyecto_id}</h2>;
-};
+import { Link, Route, Routes } from "react-router-dom";
  
 const Portafolio = () => {
   return (
@@ -22,7 +14,8 @@ const Portafolio = () => {
       </ul>
       <section>
         <Routes>
-          <Route path=":proyecto_id" element={<Proyecto />}></Route>
+          <Route path="proyecto-1" element={<h2>Proyecto 1</h2>}></Route>
+          <Route path="proyecto-2" element={<h2>Proyecto 2</h2>}></Route>
         </Routes>
       </section>
     </div>
@@ -38,14 +31,14 @@ function App() {
             <Link to="/">Inicio</Link>
           </li>
           <li>
-            <Link to="/portafolio">Portafolio</Link>
+            <Link to="/portafolio/">Portafolio</Link>
           </li>
         </ul>
       </nav>
       <section>
         <Routes>
           <Route exact path="/" element={<h1>Inicio</h1>}></Route>
-          <Route path="portafolio/*" element={<Portafolio />}></Route>
+          <Route path="/portafolio/*" element={<Portafolio />}></Route>
         </Routes>
       </section>
     </div>
@@ -53,6 +46,31 @@ function App() {
 }
  
 export default App;
+
+
+// import { Routes, Route, Link } from 'react-router-dom'
+
+// const Portafolio = () => {
+//     return (
+//         <div>
+//             <h1>Portafolio</h1>
+//             <ul>
+//                 <li>
+//                     <Link to={'/portafolio/proyecto-1'}>Proyecto 1</Link>
+//                 </li>
+//                 <li>
+//                     <Link to={'/portafolio/proyecto-2'}>Proyecto 2</Link>
+//                 </li>
+//             </ul>
+//             <div>
+//                 <Routes>
+//                     <Route path="/portafolio/proyecto-1" element={<h1>Proyecto 1</h1>} />
+//                     <Route path="/portafolio/proyecto-2" element={<h1>Proyecto 2</h1>} />
+//                 </Routes>
+//             </div>
+//         </div>
+//     )
+// }
 
 // function App() {
 //     return (
