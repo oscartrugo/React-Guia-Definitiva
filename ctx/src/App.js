ea4d27ad170f6d9ad9c2a9cd86483a81e0a3d1d6
oscartrugo <oscartrugo@gmail.com>
2022-04-04 20:12:16 -0500

    131. Primeros pasos

--- ctx/src/App.js
+++ ctx/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useContext } from 'react'
+
+//El valor por defecto se utiliza cuando no se tiene un Provider
+const ContextDefault = createContext('valor por defecto')//Recibe un valor por defecto (numero, array, object, string, etc.)
+const Context2 = createContext('valor por defecto 2')
+
+const DefaultProvider = ({children}) => {
+  return(
+    <ContextDefault.Provider value={'mi valor'}> {/**Al Context debemos pasarle un valor para pasar a los hijos mediante el provider */}
+      {children}
+    </ContextDefault.Provider>
+  )
+}
+
+const Contenido = () => {
+  const ctx = useContext(ContextDefault) //Utilizamos el contexto de Context
+  return(
+    <div>{ctx}</div>
+  )
+}
+
+const Contenido2 = () => {
+  const ctx = useContext(Context2) //Utilizamos el contexto de Context
+  return(
+    <div>{ctx}</div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DefaultProvider>
+      <Contenido/>
+      <Contenido2/>
+    </DefaultProvider>
   );
 }
 
