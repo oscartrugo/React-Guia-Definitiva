f21ff0f64b474057adc5564929a3d14af74f20bd
oscartrugo <oscartrugo@gmail.com>
2022-04-05 01:27:42 -0500

    143. Objeto history

--- router/src/6-404.js
+++ router/src/6-404.js
@@ -1,20 +1,6 @@
-import { Route, Routes, Link, useNavigate } from 'react-router-dom';
+import { Route, Routes, Link, Navigate } from 'react-router-dom';
  
 function App() {
-  const navigate = useNavigate();
- 
-  const forward = () => {
-    navigate(1);
-  };
- 
-  const back = () => {
-    navigate(-1);
-  };
- 
-  const push = (url) => {
-    navigate(url);
-  };
- 
   return (
     <div>
       <nav>
@@ -28,13 +14,16 @@ function App() {
         </ul>
       </nav>
       <section>
-        <button onClick={back}>Back</button>
-        <button onClick={forward}>Forward</button>
-        <button onClick={() => push('/chanchitofeliz')}>Push</button>
         <Routes>
           <Route exact path="/" element={<h1>Inicio</h1>}></Route>
           <Route exact path="/perfil" element={<h1>Perfil</h1>}></Route>
-          <Route path="*" element={'404 Ruta no encontrada'} />
+          <Route
+            exact
+            path="/"
+            element={<Navigate replace to="/inicio" />}
+          ></Route>
+          <Route path='*' element={<p>404: ruta no encontrada</p>}></Route>
+
         </Routes>
       </section>
     </div>
