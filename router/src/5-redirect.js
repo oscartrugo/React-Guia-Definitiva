656e966e959f5438f61477b7cb44bdd0cfcabbab
oscartrugo <oscartrugo@gmail.com>
2022-04-05 01:22:53 -0500

    142. Error 404

--- router/src/5-redirect.js
+++ router/src/5-redirect.js
@@ -1,5 +1,14 @@
 import { Route, Routes, Link, Navigate } from 'react-router-dom';
  
+const Portafolio = () => {
+  const loggedIn = false;
+ 
+  if (!loggedIn) { //si el usuario no está logeado
+    return <Navigate to="/" />; //Redirige a la raíz
+  }
+  return <h1>Portafolio</h1>;
+};
+ 
 function App() {
   return (
     <div>
@@ -11,19 +20,21 @@ function App() {
           <li>
             <Link to="/perfil">Perfil</Link>
           </li>
+          <li>
+            <Link to="/portafolio">Portafolio</Link>
+          </li>
         </ul>
       </nav>
       <section>
         <Routes>
-          <Route exact path="/" element={<h1>Inicio</h1>}></Route>
+          <Route exact path="/inicio" element={<h1>Inicio</h1>}></Route>
           <Route exact path="/perfil" element={<h1>Perfil</h1>}></Route>
+          <Route path="/portafolio" element={<Portafolio />}></Route>
           <Route
             exact
             path="/"
             element={<Navigate replace to="/inicio" />}
           ></Route>
-          <Route path='*' element={<p>404: ruta no encontrada</p>}></Route>
-
         </Routes>
       </section>
     </div>
