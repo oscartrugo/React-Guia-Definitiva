5e52d274494047f8564525dab2aca74cce1e5349
oscartrugo <oscartrugo@gmail.com>
2022-04-04 23:48:15 -0500

    138. Rutas anidadas

--- router/src/App.js
+++ router/src/App.js
@@ -1,26 +1,48 @@
 import { Routes, Route, Link } from 'react-router-dom'
 
+const Portafolio = () => {
+    return (
+        <div>
+            <h1>Portafolio</h1>
+            <ul>
+                <li>
+                    <Link to={'/portafolio/proyecto-1'}>Proyecto 1</Link>
+                </li>
+                <li>
+                    <Link to={'/portafolio/proyecto-2'}>Proyecto 2</Link>
+                </li>
+            </ul>
+            <div>
+                <Routes>
+                    <Route path="/portafolio/proyecto-1" element={<h1>Proyecto 1</h1>} />
+                    <Route path="/portafolio/proyecto-2" element={<h1>Proyecto 2</h1>} />
+                </Routes>
+            </div>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Inicio</Link> {/**Captura el efecto de abrir otro link */}
-          </li>
-          <li>
-            <Link to='/portafolio'>Portafolio</Link> {/**Captura el efecto de abrir otro link */}
-          </li>
-        </ul>
-      </nav>
-      <section>
-        <Routes>
-          <Route exact path='/' element={<h1>Inicio</h1>} />
-          <Route exact path='/portafolio' element={<h1>Portafolio</h1>}/>
-        </Routes>
-      </section>
-    </div>
-  );
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/'>Inicio</Link> {/**Captura el efecto de abrir otro link */}
+                    </li>
+                    <li>
+                        <Link to='/portafolio'>Portafolio</Link> {/**Captura el efecto de abrir otro link */}
+                    </li>
+                </ul>
+            </nav>
+            <section>
+                <Routes>
+                    <Route path='/' element={<h1>Inicio</h1>} />
+                    <Route path='/portafolio/*' element={<Portafolio />} />
+                </Routes>
+            </section>
+        </div>
+    );
 }
 
 export default App;
